72a80b4e481397e34ea8de0f2d42f3a3c2ba8800
PritchardMap <pritchardmapfumo@gmail.com>
2021-04-30 23:23:01 +0200

    Navbar Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HostComponent } from './core/layout/host/host.component';
+import { HeaderComponent } from './core/layout/header/header.component';
+import { HomeComponent } from './core/components/home/home.component';
+import { AppRoutingModule } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HostComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
